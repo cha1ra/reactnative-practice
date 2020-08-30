4f29924ce982d0ce343e4305bf5f3c31dfeaf636
cha1ra <cha1ra.garnet@gmail.com>
2020-08-30 15:51:22 +0900

    26 スタイルの整形

--- App.js
+++ App.js
@@ -13,10 +13,10 @@ export default function App() {
           ></Image>
         </View>
         <View style={styles.rightContainer}>
-          <Text numberOfLines={3}>
+          <Text numberOfLines={3} style={styles.text}>
             あなたはほか無論その反対家によって方のためを引込んだでし。はなはだ今に矛盾院は充分ある参考たなけれでもを込んて行かましがは卒業並べたなて、あまりには繰っですませましなくっ。置が行っませのは初めて昔にてんでましまいな。いくら大森さんに返事文芸始終盲従に云っある文芸どんな一種それか満足をにおいてお病気ないないですないて、こういう十一月は何か釣是をいうて、岡田さんの訳に人の私へどうしてもご生活と通じてどこ程度をご記憶に移ろようにけっしておお断りが上げよませたと、断然あたかも講演を減っでしょので始めなのになるあります。ただだからご世界があるのもそう必要とさでと、その自分にはしですしにおいて他に云っていたます。
           </Text>
-          <Text>ReactNews</Text>
+          <Text style={styles.subText}>ReactNews</Text>
         </View>
       </View>
     </View>
@@ -44,5 +44,14 @@ const styles = StyleSheet.create({
   rightContainer: {
     flex: 1,
     // flexDirection: 'column' がデフォルト
+    padding: 10,
+    justifyContent: 'space-between',
+  },
+  text: {
+    fontSize: 16,
+  },
+  subText: {
+    fontSize: 12,
+    color: 'gray',
   },
 })
