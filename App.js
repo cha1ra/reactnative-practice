d10d5b51400cf3c2d993ca72bffd96beee255742
cha1ra <cha1ra.garnet@gmail.com>
2020-08-30 17:14:30 +0900

    48-1: StackNavigationの設定

--- App.js
+++ App.js
@@ -1,73 +1,21 @@
 import { StatusBar } from 'expo-status-bar'
-import React, { useState, useEffect } from 'react'
-import { StyleSheet, View, FlatList, SafeAreaView } from 'react-native'
-import ListItem from './components/ListItem'
-import dummyArticles from './dummies/articles.json'
-import Constants from 'expo-constants'
-import axios from 'axios'
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import HomeScreen from './screens/HomeScreen'
 
-const URL = `http://newsapi.org/v2/top-headlines?country=jp&category=business&apiKey=${Constants.manifest.extra.newsApiKey}`
+const Stack = createStackNavigator()
 
 export default function App() {
-  const [articles, setArticles] = useState([])
-  // 第２引数に空配列を渡すと最初だけ実行する
-  useEffect(() => {
-    fetchArticles()
-  }, [])
-
-  const fetchArticles = async () => {
-    try {
-      const response = await axios.get(URL)
-      setArticles(response.data.articles)
-    } catch (error) {
-      console.error(error)
-    }
-  }
-
   return (
-    <SafeAreaView style={styles.container}>
-      <FlatList
-        data={articles}
-        renderItem={({ item }) => (
-          <ListItem
-            imageUrl={item.urlToImage}
-            title={item.title}
-            author={item.author}
-          />
-        )}
-        keyExtractor={(item, index) => index.toString()}
-      />
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  itemContainer: {
-    height: 100,
-    width: '100%',
-    borderColor: 'gray',
-    borderWidth: 1,
-    // こいつが横並びにするのに必要
-    flexDirection: 'row',
-  },
-  leftContainer: {
-    width: 100,
-  },
-  rightContainer: {
-    flex: 1,
-    // flexDirection: 'column' がデフォルト
-    padding: 10,
-    justifyContent: 'space-between',
-  },
-  text: {
-    fontSize: 16,
-  },
-  subText: {
-    fontSize: 12,
-    color: 'gray',
-  },
-})
---
+++ screens/HomeScreen.js
@@ -0,0 +1,72 @@
+import { StatusBar } from 'expo-status-bar'
+import React, { useState, useEffect } from 'react'
+import { StyleSheet, View, FlatList, SafeAreaView } from 'react-native'
+import ListItem from '../components/ListItem'
+import Constants from 'expo-constants'
+import axios from 'axios'
+
+const URL = `http://newsapi.org/v2/top-headlines?country=jp&category=business&apiKey=${Constants.manifest.extra.newsApiKey}`
+
+export default HomeScreen = () => {
+  const [articles, setArticles] = useState([])
+  // 第２引数に空配列を渡すと最初だけ実行する
+  useEffect(() => {
+    fetchArticles()
+  }, [])
+
+  const fetchArticles = async () => {
+    try {
+      const response = await axios.get(URL)
+      setArticles(response.data.articles)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={articles}
+        renderItem={({ item }) => (
+          <ListItem
+            imageUrl={item.urlToImage}
+            title={item.title}
+            author={item.author}
+          />
+        )}
+        keyExtractor={(item, index) => index.toString()}
+      />
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  itemContainer: {
+    height: 100,
+    width: '100%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    // こいつが横並びにするのに必要
+    flexDirection: 'row',
+  },
+  leftContainer: {
+    width: 100,
+  },
+  rightContainer: {
+    flex: 1,
+    // flexDirection: 'column' がデフォルト
+    padding: 10,
+    justifyContent: 'space-between',
+  },
+  text: {
+    fontSize: 16,
+  },
+  subText: {
+    fontSize: 12,
+    color: 'gray',
+  },
+})
