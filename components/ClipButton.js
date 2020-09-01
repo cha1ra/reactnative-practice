bf2a5796e13b0d827f0ffe314694239a378a8fd9
cha1ra <cha1ra.garnet@gmail.com>
2020-09-01 23:14:16 +0900

    fix: typo

--- components/ClipButton.js
+++ components/ClipButton.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { StyleSheet, TouchableOpacity } from 'react-native'
-import { FontAwesome } from '@export/vector-icons'
+import { FontAwesome } from '@expo/vector-icons'
 
 const styles = StyleSheet.create({
   container: {
