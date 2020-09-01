9cd0ecd6f129c23acbcd997e5f3e772e89f9bd9c
cha1ra <cha1ra.garnet@gmail.com>
2020-09-01 23:24:52 +0900

    67: redux の永続化

--- App.js
+++ App.js
@@ -2,12 +2,15 @@ import { StatusBar } from 'expo-status-bar'
 import React from 'react'
 import AppNavigator from './navigation/AppNavigator'
 import { Provider } from 'react-redux'
-import store from './store'
+import store, { persistor } from './store'
+import { PersistGate } from 'redux-persist/integration/react'
 
 export default function App() {
   return (
     <Provider store={store}>
-      <AppNavigator />
+      <PersistGate loading={null} persistor={persistor}>
+        <AppNavigator />
+      </PersistGate>
     </Provider>
   )
 }
